9a70264e39793c06c5e5b6842af18decddffd042
Marcelo Agustini <marceloagustini@iMac-de-Marcelo.local>
2023-11-06 17:27:21 -0300

    change port

--- src/api/products.api.js
+++ src/api/products.api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = 'http://localhost:3001/api';
 
 export const getAllProducts = (searchTerm) => {
     return axios.get(`${BASE_URL}/products?name=${searchTerm || ''}`);
