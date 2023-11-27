659e5f7ccad9e04d1a9ceb1781c96e754793e274
Marcelo Agustini <marceloagustini@iMac-de-Marcelo.local>
2023-11-27 20:02:01 -0300

    eliminar y editar

--- src/api/products.api.js
+++ src/api/products.api.js
@@ -1,13 +1,39 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:3001';
+const access_token = localStorage.getItem('token');
 
 export const getAllProducts = (searchTerm) => {
     return axios.get(`${BASE_URL}/api/products?name=${searchTerm || ''}`);
 };
 
 export const createProduct = (product) => {
-    return axios.post(`${BASE_URL}/api/products`, product);
+    return axios.post(`${BASE_URL}/api/products`, product,
+        {
+            headers: {
+                'Authorization': `${access_token}`
+            }
+        }
+    );
+};
+
+//UPDATE AND DELETE
+export const updateProduct = (id, product) => {
+    return axios.put(`${BASE_URL}/api/products/${id}`, product,
+        {
+            headers: {
+                'Authorization': `${access_token}`
+            }
+        });
+};
+
+export const removeProduct = (id) => {
+    return axios.delete(`${BASE_URL}/api/products/${id}`,
+        {
+            headers: {
+                'Authorization': `${access_token}`
+            }
+        });
 };
 
 
@@ -16,4 +42,4 @@ export const login = (loginData) => {
     return axios.post(`${BASE_URL}/auth/login`, loginData);
 };
 
- 
+
--- src/pages/panel/PanelPage.jsx
+++ src/pages/panel/PanelPage.jsx
@@ -1,15 +1,44 @@
 
 import { getAllProducts } from '../../api/products.api';
 import { useEffect, useState } from 'react';
-import { Container, TableRow, Table, Avatar, TableBody, TableCell, TableHead, TableContainer, Paper } from '@mui/material';
+import { Container, TableRow, Table, Avatar, TableBody, TableCell, TableHead, TableContainer, Paper, IconButton } from '@mui/material';
 import Loader from '../../components/Loader'
 import FormProduct from '../../components/FormProduct';
+import DeleteIcon from '@mui/icons-material/Delete';
+import UpdateIcon from '@mui/icons-material/Edit';
+import { removeProduct } from '../../api/products.api';
 
 
 
 function HomePagePanel() {
     const [products, setProducts] = useState([]);
     const [load, setLoad] = useState(false);
+    const [open, setOpen] = useState(false); // controlar el popup de producto
+
+    const [product, setProduct] = useState({
+        name: '',
+        catalog: '',
+        image: '',
+        description: '',
+        price: 0,
+        stock: 0
+    });
+
+    const handleClickOpen = () => {
+        setProduct({
+            name: '',
+            catalog: '',
+            image: '',
+            description: '',
+            price: 0,
+            stock: 0
+        })
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     useEffect(() => {
         fetchData();
@@ -23,9 +52,24 @@ function HomePagePanel() {
         });
     }
 
+    const handleRemoveItem = (id) => {
+        removeProduct(id).then((response) => {
+            if (response) {
+                fetchData()
+            }
+        }).catch((error) => {
+            alert(error);
+        });
+    }
+
+    const handleUpdateItem = (item) => {
+        setProduct(item);
+        setOpen(true);
+    }
+
     return (
         <Container>
-            <FormProduct fetchData={fetchData}></FormProduct>
+            <FormProduct product={product} handleClose={handleClose} handleClickOpen={handleClickOpen} open={open} fetchData={fetchData}></FormProduct>
             <br />
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -51,12 +95,18 @@ function HomePagePanel() {
                                 <TableCell align="right">$ {row.price}</TableCell>
                                 <TableCell align="right">{row.catalog}</TableCell>
                                 <TableCell align="right">
-
+                                    <IconButton color="inherit" aria-label="Editar" onClick={() => handleUpdateItem(row)}>
+                                        <UpdateIcon />
+                                    </IconButton>
+                                    <IconButton color="inherit" aria-label="Eliminar" onClick={() => handleRemoveItem(row._id)} >
+                                        <DeleteIcon />
+                                    </IconButton>
                                 </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
+                <br /><br /><br />
             </TableContainer>
         </Container>
     )
